25742a416aa2bf0a1796ec20805c4b43da4141cc
Aragubas <47148297+aragubas@users.noreply.github.com>
2021-04-26 23:45:52 -0300

    Added utils class

---
+++ TaiyouUI/Utils.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TaiyouUI
+{
+    public static class Utils
+    {
+        public static void CreateWindow(taiyouUserControl control, string DefaultTitle = "Untitled Window", bool ShowAsDialog = false)
+        {
+            TabNotepadForm ParentForm = new TabNotepadForm();
+
+            // Add controls to the form
+            control.RootForm = ParentForm;
+            ParentForm.FormControls.Controls.Add(control);
+
+            ParentForm.Text = DefaultTitle;
+
+            if (!ShowAsDialog)
+            {
+                ParentForm.Show();
+
+            }
+            else { ParentForm.ShowDialog(); }
+
+        }
+
+    }
+}
